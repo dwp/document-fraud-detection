7f8b2adfee12d2b15ffcd749c80442e5f6686a1d
Claire Rooney <claire.rooney@engineering.digital.dwp.gov.uk>
2026-07-10 14:04:22 +0100

    Hybrid localisation
    Updates to localisation to show highlights
    
    GitOrigin-RevId: 31017106e8dfca4ec91ac6c7f8e81d37cf66274a

--- app/routes.js
+++ app/routes.js
@@ -64,6 +64,68 @@ router.post('/prototypes/DFP-1087/v1/issue-delete', function(request, response) 
     response.redirect('issue-list');
 });
 
-router.post('/prototypes/DFP-1087/v1/issue-consent', function(request, response) {
+router.post('/prototypes/DFP-1320/issue-consent', function(request, response) {
+        response.redirect("issue-confirm");
+});
+
+
+router.post('/prototypes/DFP-1320/issue-feedback', function(request, response) {
+    if(request.session.data['reportedType']=='False positive'){
+        response.redirect("issue-p-location");
+    }
+    else{
+        response.redirect("issue-n-location");
+    }
+});
+
+router.post('/prototypes/DFP-1320/issue-p-location', function(request, response) {
+        response.redirect("issue-describe");
+});
+
+router.post('/prototypes/DFP-1320/issue-n-location', function(request, response) {
+   
+        response.redirect("issue-describe");
+});
+
+router.post('/prototypes/DFP-1320/issue-describe', function(request, response) {
+        response.redirect("issue-cya");
+});
+
+
+router.post('/prototypes/DFP-1320/issue-cya', function(request, response) {
+// This is where we save the array objects in each loop
+        let tempArray = [];
+        if(request.session.data['issues']){
+            tempArray = request.session.data['issues'];
+         }
+         let tempCheckName = request.session.data['checkName'] +": "+ request.session.data['titleOfIssue'];
+         let tempObject = {type: request.session.data['reportedType'], page: request.session.data['pageNo'], location: request.session.data['location'], check: tempCheckName, detail: request.session.data['moreDetail']};
+         tempArray.push(tempObject);
+         request.session.data['issues'] = tempArray;
+        response.redirect("issue-list");
+});
+
+router.post('/prototypes/DFP-1320/issue-list', function(request, response) {
+      if(request.session.data['addAnother']=='Yes'){
+        response.redirect("issue-feedback");
+    }
+    else{
+        response.redirect("issue-consent");
+    }
+});
+
+router.post('/prototypes/DFP-1320/issue-delete', function(request, response) {
+    if(request.session.data['delete']=='yes'){  
+      let x = request.session.data['loop'];
+      let temporaryArray = [];
+      temporaryArray = request.session.data['issues']
+      temporaryArray.splice(x, 1);
+      request.session.data['issues'] = temporaryArray
+      request.session.data['loop'] = null;  
+    }
+    response.redirect('issue-list');
+});
+
+router.post('/prototypes/DFP-1320/issue-consent', function(request, response) {
         response.redirect("issue-confirm");
 });
